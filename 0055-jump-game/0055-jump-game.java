74c9f0faf00d65c757ba4ca43b13065e63a79aca
Rishav Pramanik <91870195+aurus18@users.noreply.github.com>
2024-09-18 00:07:26 +0530

    Time: 2 ms (76.13%), Space: 45.8 MB (22.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canJump(int[] nums) { //nums = [2,3,1,1,4]
+        // if (nums.length <= 1){
+        //     return false;
+        // }
+        // for (int i = 0; i < nums.length; i++){
+        //     if ( i > 0 ){
+        //         int c = nums [i]; //c=3 when i=1
+        //         if (c == (nums.length-i-1)){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        // My approach is not working as i was accouting for an exact match rather than incremental value
+        // and so it values of arrays like [0]
+        // Method - 2 : using maxReach
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++){
+
+            if (i > maxReach){
+                return false;
+            }
+            // Update maxReach
+            maxReach = Math.max(maxReach, i + nums[i]);
+            // maxReach = 0, Math.max finds the larger among the two and updates accordingly
+
+            // If we can reach or go beyond the last index, return true
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
